33d8edeabf6622b4c28961e71434ad4184313308
Hee Su Lee <hesue615@gmail.com>
2021-09-28 22:26:19 +0900

    initial commit

---
+++ src/main/java/com/tistory/heesutory/HeesutoryApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.heesutory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeesutoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeesutoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tistory/heesutory/HeesutoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.heesutory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeesutoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
